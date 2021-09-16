da47e3b2274e210b0d80f06a207189141c20a206
isaac lynch <thebunnyarcher@gmail.com>
2021-09-16 17:17:16 -0400

    Initial commit

---
+++ src/main/java/exercise16/solution16.java
@@ -0,0 +1,24 @@
+package exercise16;
+
+
+import java.util.Scanner;
+
+public class solution16{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How old are you? ");
+        int age = sc.nextInt();
+        String result = (age > 15) ? "You are old enough to legally drive." : "You are not old enough to legally drive.";
+        System.out.println(result);
+
+    }
+}
+/*
+FINALLY AN EASY ONE TO BREAK UP THE INSANITY. very easy, first the scanner function is imported.
+after that the main function has the solution because its only four lines of code. first the
+user puts in their age which is stored in "age", after that the result is output through a
+ternary operator which sees whether or not the user's age is below or above 16. if it is the
+program prints that you are old enough to drive, otherwise it prints that you are not.
+ */
