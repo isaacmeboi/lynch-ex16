8efc1cf2900054fb28240ca88a006d79943d5434
isaac lynch <thebunnyarcher@gmail.com>
2021-09-17 22:21:09 -0400

    added copyright

--- src/main/java/exercise15/solution15.java
+++ src/main/java/exercise15/solution15.java
@@ -1,6 +1,6 @@
 /*
- *  UCF COP3330 Fall 2021 Assignment 15 Solution
- *  Copyright 2021 Isaac Lynch
+   UCF COP3330 Fall 2021 Assignment 15 Solution
+   Copyright 2021 Isaac Lynch
  */
 
 package exercise15;
